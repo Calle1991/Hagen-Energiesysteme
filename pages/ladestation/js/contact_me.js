4a0e21aa0b6dda1ed82e619dd4cfe921f167af4c
Pascal Tobinski <pascal.tobinski@gmx.de>
2021-01-27 17:23:13 +0100

    SPAM-Schutz (Ladestation)
    - Alten Fragebogen entfernt
    - SPAM-Schutz auf Ladestation eingerichtet

--- pages/ladestation/js/contact_me.js
+++ pages/ladestation/js/contact_me.js
@@ -11,18 +11,26 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var message = $("textarea#message").val();
+            var antispam = $("#antispam").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+
+            console.log(name);
+            console.log(email);
+            console.log(antispam);
+            console.log(firstName);
+
             $.ajax({
                 url: "./mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
-                    message: message
+                    message: message,
+                    antispam: antispam
                 },
                 cache: false,
                 success: function() {
@@ -31,7 +39,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Nachricht wurde gesendet! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -43,7 +51,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", Der Emailserver scheint nicht erreichbar zu sein. Probiere es später noch einmal");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
