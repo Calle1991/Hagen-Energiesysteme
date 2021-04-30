c9c7211ff49c97c9b248b8117531f2c0c4567a2c
Pascal Tobinski <pascal.tobinski@gmx.de>
2021-04-30 11:47:13 +0200

    Validation von Radiobuttons
    - Kartenauswahl
    - Text einblenden bei Auswahl

--- pages/ladestation/js/main.js
+++ pages/ladestation/js/main.js
@@ -12,3 +12,35 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+//Card - Validation
+
+$('#privat').click(function(){
+  $('#stepOne__btn').addClass('active');
+  $('#stepOne').css('max-height','100%');
+});
+
+
+$('#KFW__Nein').click(function(){
+  $('.infoBox__KFW').css('visibility','visible');
+});
+
+
+//Radiobutton Validation - Accordion
+$('#KFW__Nein').click(function(){
+  $('.infoBox__KFW').css('visibility','visible');
+});
+
+$('#KFW__Ja').click(function(){
+  $('.infoBox__KFW').css('visibility','hidden');
+});
+
+
+$('#GRUEN__Nein').click(function(){
+  $('.infoBox__gruenStrom').css('visibility','visible');
+});
+
+$('#GRUEN__Ja').click(function(){
+  $('.infoBox__gruenStrom').css('visibility','hidden');
+});
